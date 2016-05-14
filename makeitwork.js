e177c07d4086b296fa0d4c99ae02df2760697e5b
Jonathan56 <jonathan.coignard@gmail.com>
2016-05-14 13:27:14 -0700

    show the curve

--- makeitwork.js
+++ makeitwork.js
@@ -3,8 +3,6 @@ var margin = {top: 30, right: 50, bottom: 70, left: 100},
     width = 1200 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parsedData;
-
 // Parsing date
 var parseDate = d3.time.format.utc("%Y-%m-%dT%H:%M:%S").parse;
 
@@ -55,26 +53,28 @@ svg.append("g")
     .style("text-anchor", "end")
     .text("Power demand (unit)");
 
-// // append clip path for lines plotted, hiding those part out of bounds
-// svg.append("defs")
-//     .append("clipPath") 
-//     .attr("id", "clip")
-//     .append("rect")
-//     .attr("width", width)
-//     .attr("height", height);
+// append clip path for lines plotted, hiding those part out of bounds
+svg.append("defs")
+    .append("clipPath") 
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
 
 // Load the data
 var myYDomain, myXDomain;
+var toplot = [];
+var mydata = [];
 url = "https://utilityapi.com/api/services/48492/intervals.json?start=2016-05-12&end=2016-05-13&access_token=3f7a4d2a86a34f958e63e8e566da57d7"
 d3.json(url, function(error, data) {
 
 // parsing the date in d3 format
 data.forEach(function(d) {
     d.interval_start = parseDate(d.interval_start.substring(0, 19)),
-    d.values = {time: d.interval_start, data: d.interval_kW}
+    toplot.push({time: d.interval_start, data: d.interval_kW})
 });
 
-parsedData = data;
+mydata.push(toplot)
 
 myYDomain = d3.extent(data, function(d) { return d.interval_kW; })
 power_scale.domain(myYDomain);
@@ -86,14 +86,14 @@ svg.select(".xAxis").call(xAxis);
 
 // select all locations and bound group to non existant location
 power_demand_svg = svg.selectAll(".power_curve")
-                .data(data)
+                .data(mydata)
                 .enter().append("g")
                 .attr("class", "power_curve");
 
 // in each group append a path generator for lines and give it the bounded data 
 power_demand_svg.append("path")
     .attr("class", "line")
-    // .attr("clip-path", "url(#clip)")
+    .attr("clip-path", "url(#clip)")
     .attr("d", function(d) {
-    	return power_demand_line(d.values); });
+    	return power_demand_line(d); });
 });
