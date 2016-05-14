6481037b088167bd11c0085c0e15b880f530f700
Jonathan56 <jonathan.coignard@gmail.com>
2016-05-14 15:34:07 -0700

    almost done

--- makeitwork.js
+++ makeitwork.js
@@ -62,18 +62,17 @@ svg.append("defs")
     .attr("height", height);
 
 // Initialize slider
-var slider = d3.slider().min(0).max(100).ticks(10).showRange(true).value(50).callback(function(evt) {
-        console.log('callback: ' + self.slider.value());
-      });
+var slider;
 
-// Render the slider in the div
-d3.select('#slider').call(slider);
 //d3.slider().on("slide", function(evt, value) {console.log(value)});
 
 // Load the data
-var myYDomain, myXDomain;
+var myYDomain, myXDomain, power_demand_svg;
 var toplot = [];
+var toplot2 = [];
 var mydata = [];
+var sum = 0;
+var sumpeak = 0;
 url = "https://utilityapi.com/api/services/48492/intervals.json?start=2016-05-12T00:00:00%2D07:00&end=2016-05-13T07:00:00%2D07:00&access_token=3f7a4d2a86a34f958e63e8e566da57d7"
 d3.json(url, function(error, data) {
 
@@ -81,13 +80,31 @@ d3.json(url, function(error, data) {
 data.forEach(function(d) {
     d.interval_start = parseDate(d.interval_start.substring(0, 19)),
     toplot.push({time: d.interval_start, data: d.interval_kW})
+    toplot2.push({time: d.interval_start, data: d.interval_kW})
 });
 
 mydata.push({color: "blue", value: toplot})
-mydata.push({color: "red", value: toplot.slice(0)})
+mydata.push({color: "red", value: toplot2})
 
-myYDomain = d3.extent(data, function(d) { return d.interval_kW; })
-power_scale.domain(myYDomain);
+// Initialize ratio
+for (i = 0; i < mydata[1].value.length; i++)
+{
+    if ((mydata[1].value[i].time.getHours()) >= 15
+        && (mydata[1].value[i].time.getHours()) <= 20) {
+        sumpeak = sumpeak + mydata[1].value[i].data;
+    }
+        sum = sum + mydata[1].value[i].data;
+}
+
+slider = d3.slider().min(0).max(50).ticks(10).showRange(true).value(100 * sumpeak / sum).callback(function(evt) {
+        console.log('callback: ' + self.slider.value());
+        update_power_demand(self.slider.value())
+      });
+// Render the slider in the div
+d3.select('#slider').call(slider);
+
+// myYDomain = d3.extent(data, function(d) { return d.interval_kW; })
+power_scale.domain([0, 0.6]);
 svg.select(".yAxis").call(yAxis);
 
 myXDomain = d3.extent(data, function(d) { return d.interval_start; })
@@ -109,4 +126,33 @@ power_demand_svg.append("path")
     .style("stroke", function(d) { return d.color; });
 });
 
-function 
+function update_power_demand(slider_value){
+	transitionTime = 0;
+	roriginal = sumpeak / sum;
+	rnew = slider_value / 100;
+
+	for (i = 0; i < mydata[1]['value'].length; i++){
+
+        if ((mydata[1].value[i].time.getHours()) >= 15
+            && (mydata[1].value[i].time.getHours()) <= 20) {
+            mydata[1].value[i].data = rnew / roriginal * mydata[0].value[i].data;
+        }
+        else
+        {
+            mydata[1].value[i].data = (1 - rnew) / (1 - roriginal) * mydata[0].value[i].data;
+        }
+    }
+
+	// // Update y axis with new data
+	// myYDomain1 = d3.extent(mydata[0].value, function(d) { return d.data; })
+	// myYDomain2 = d3.extent(mydata[1].value, function(d) { return d.data; })
+	// minY = d3.min([myYDomain1[0], myYDomain2[0]])
+	// maxY = d3.max([myYDomain1[1], myYDomain2[1]])
+	// power_scale.domain([minY, maxY]);
+	// svg.select(".yAxis").call(yAxis);
+
+	// Update line
+	power_demand_svg.select(".line")
+	  .transition(transitionTime)
+	  .attr("d", function(d) { return power_demand_line(d.value); });
+}
